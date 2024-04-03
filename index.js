186dc73580652787dd9f4a98a4c03fe6bf4c9a2a
Gideon Mutai <giddymutai13@gmail.com>
2024-04-03 14:53:35 +0300

    Adds local scope

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declaring a variable in global scope
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'John Doe'
+
+// A function that converts the customer name to uppercase
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+// A function that sets the bestCustomer global variable
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+// A function that overrides the bestCustomer variable
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+// A function that changes the least favorite customer
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Jane Doe'
+}
+
+
